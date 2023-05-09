2c78cee52669a17d826c5d02e17bf96a5146f211
Asuka <yalingliu8888@gmail.com>
2023-05-08 23:24:16 -0700

    implement auth routes

--- web-chat/server/routes/authRoutes.js
+++ web-chat/server/routes/authRoutes.js
@@ -1,14 +1,78 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 const router = express.Router();
 
-// User registration
 router.post('/register', async (req, res) => {
-    // Register the user and return a token, or return an error if the registration fails.
-});
+    try {
+      // Validate user input
+      const { username, password, email } = req.body;
+      if (!username || !password || !email) {
+        return res.status(400).json({ message: 'All fields are required' });
+      }
+  
+      // Check if the user already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+  
+      // Hash the password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+  
+      // Create a new user
+      const newUser = new User({
+        username,
+        password: hashedPassword,
+        email,
+      });
+  
+      await newUser.save();
+  
+      // Generate a JWT
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+      // Send the JWT in the response
+      res.status(201).json({ token });
+  
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
 
-// User login
 router.post('/login', async (req, res) => {
-    // Authenticate the user and return a token, or return an error if the authentication fails.
-});
-
-module.exports = router;
+    try {
+      // Validate user input
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res.status(400).json({ message: 'All fields are required' });
+      }
+  
+      // Check if the user exists
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: 'User not found' });
+      }
+  
+      // Verify the user's password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+      }
+  
+      // Generate a JWT
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+      // Send the JWT in the response
+      res.status(200).json({ token });
+  
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
+  
+  module.exports = router;
